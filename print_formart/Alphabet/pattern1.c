16c819fb249c71d6915602ee4128907387e4a541
collins <collinsboniface1000@gmail.com>
2021-09-11 18:06:56 +0300

    The complition of the alphabet pattern1.c

--- print_formart/Alphabet/pattern1.c
+++ print_formart/Alphabet/pattern1.c
@@ -6,8 +6,13 @@ int main() {
 	int i;
 	//Inner Loop
 	int j;
+	//Number of rows entered by the user 
+	int rows;
 
-	for (i = 1; i <= 5; i++) {
+	printf("Enter number of rows you want to print:");
+	scanf("%d", &rows);
+
+	for (i = 1; i <= rows; i++) {
 
 		for (j = 1; j <= i; j++) {
 			printf("%c", j + 64);
