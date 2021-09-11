4c9e46f04837a89d233bf30bac728018273373e9
collins <collinsboniface1000@gmail.com>
2021-09-11 17:56:31 +0300

    The first alphabe in the folder

---
+++ print_formart/Alphabet/alpha1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+	
+	//Outer Loop
+	int i;
+	//Inner Loop
+	int j;
+
+	for (i = 1; i <= 5; i++) {
+
+		for (j = 1; j <= i; j++) {
+			printf("%c", j + 64);
+		}
+		printf("\n");
+	}
+
+}
