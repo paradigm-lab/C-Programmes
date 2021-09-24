f80562a82cc7c7dd42381e708fef97750a738755
collins <collinsboniface1000@gmail.com>
2021-09-24 18:30:52 +0300

    The update to the Arithmetic program in the implementation of calculating the specified part in the Arithmetic option done with the Addition part

--- Arithmetic.c
+++ Arithmetic.c
@@ -1,18 +1,24 @@
 #include <stdio.h>
 //The prototype of the function 
 float add(float, float);
+void Addition();
 float sub(float, float);
 float mult(float , float);
 float div(float, float);
 
 int main ()
-{   
-      //Addition
-      printf("Addition for two numbers: \n");
-      int Add1, Add2;
-      scanf("\n\n\n%d", &Add1);
-      scanf("\n\n\n%d", &Add2);
-      printf("Sum: %f \n\n",(float) add(Add1, Add2));
+{ 
+	printf("\t\t Welcome to the Mathematics world\n\n");
+	printf("Enter 1 for Addition, Enter 2 for Subtraction, Enter 3 for Multiplication, Enter 4 for Division: \n\n");
+	int input;
+        scanf("%d", &input);	
+        
+	if (input == 1) {
+		Addition();
+	}
+	else if (input == 2) {
+
+	}
 
       //Subtraction
       printf("Subtraction for two numbers: \n");
@@ -36,11 +42,22 @@ int main ()
       printf("Division: %f", (float) div(div2, div1));
 
 }
+void Addition(){
+	printf("The addition of two numbers: \n");
+	int Add1, Add2;
+	scanf("\n\n\n %d", &Add1);
+	scanf("\n\n\n %d", &Add2);
+	printf("Sum: %f \n \n", (float) add(Add1, Add2));
+}
+
+void Subtraction() {
+	printf("The Subtraction for");
+}
 
 // The function to add two numbers 
 float add(float a, float b)
   {
-      return a + b;
+      	return a + b;
   }
 
 // The function to return the subtraction of the two numbers
