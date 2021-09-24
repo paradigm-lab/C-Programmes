a47c7e9c1744478acc829a6e9ef8968d78f54cfc
collins <collinsboniface1000@gmail.com>
2021-09-24 21:02:12 +0300

    The update in the Arithmetic.c with option part in the calculating program and making of the new function for executing either Addition, Subtraction, Multiplication and Division

--- Arithmetic.c
+++ Arithmetic.c
@@ -2,58 +2,77 @@
 //The prototype of the function 
 float add(float, float);
 void Addition();
+
 float sub(float, float);
+void Subtraction();
+
 float mult(float , float);
+void Multiplication();
+
 float div(float, float);
+void Division();
 
 int main ()
 { 
 	printf("\t\t Welcome to the Mathematics world\n\n");
 	printf("Enter 1 for Addition, Enter 2 for Subtraction, Enter 3 for Multiplication, Enter 4 for Division: \n\n");
+
 	int input;
         scanf("%d", &input);	
         
-	if (input == 1) {
+	switch (input) {
+	
+	case 1:
 		Addition();
+		break;
+	case 2:
+		Subtraction();
+		break;
+	case 3: 
+		Multiplication();		
+		break;
+	case 4:
+		Division();
+		break;
+	default:
+		printf("\t\t\tINVALID VALUE\n");	
+		break;
 	}
-	else if (input == 2) {
 
-	}
-
-      //Subtraction
-      printf("Subtraction for two numbers: \n");
-      int sub1, sub2;
-      scanf("\n\n\n %d", &sub1);
-      scanf("\n\n\n %d", &sub2);
-      printf("Subtraction: %f \n\n", (float) sub(sub1, sub2));
-
-      //Multiplication
-      printf("Multiplication for two numbers: \n");
-      int mult1, mult2;
-      scanf("%d", &mult1);
-      scanf("%d", &mult2);
-      printf("Multiplication: %f \n\n", (float) mult(mult1, mult2));
-
-      //Division
-      printf("Division of the two numbers: \n");
-      int div1, div2;
-      scanf("%d", &div1);
-      scanf("%d", &div2);
-      printf("Division: %f", (float) div(div2, div1));
-
+ 	printf("Welcome to the Mathematician world");
 }
+
 void Addition(){
 	printf("The addition of two numbers: \n");
 	int Add1, Add2;
-	scanf("\n\n\n %d", &Add1);
-	scanf("\n\n\n %d", &Add2);
+	scanf("\t\t %d", &Add1);
+	scanf("\t\t %d", &Add2);
 	printf("Sum: %f \n \n", (float) add(Add1, Add2));
 }
 
 void Subtraction() {
-	printf("The Subtraction for");
+	printf("The Subtraction of two numbers: \n");
+	int sub1, sub2;
+	scanf("\t\t %d", &sub1);
+	scanf("\t\t %d", &sub2);
+	printf("Subtraction: %f \n \n", (float) sub(sub1, sub2));
 }
 
+void Multiplication() {
+	printf("The Multiplication of the two number: \n");
+	int mult1, mult2;
+	scanf("%d", &mult1);
+	scanf("%d", &mult2);
+	printf("Multiplication: %f \n \n", (float) mult(mult1, mult2));
+}
+
+void Division() {
+	printf("Division of the two numbers: \n");
+	int div1, div2;
+	scanf("%d", &div1);
+	scanf("%d", &div2);
+	printf("Division: %f \n\n", (float) div(div1, div2));
+}
 // The function to add two numbers 
 float add(float a, float b)
   {
