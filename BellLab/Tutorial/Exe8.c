dd5abf2e632aa80c9edec0a8160fce24d6962c2d
collins <collinsboniface1000@gmail.com>
2021-10-13 22:39:01 +0300

    Updating the Exe8.c program by lating the unformatted input use the While loop to iterate in the loop and increment the variable and out of the while loop scope i print the incrementing variable by getting the total number of words inputed at the runtime of the program

---
+++ BellLab/Tutorial/Exe8.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+	//The program to count the characters in the input "Version 1"
+	
+	long nc;
+	int c;
+	printf("Enter the characters: \n");
+	/*	
+	//Using the do while loop
+	do {
+		c = getchar();
+		++nc;
+	} while (c != EOF);
+	printf("%d", nc);
+	*/
+
+	//Using the While loop to count the charactes
+	while((c = getchar()) != EOF) {
+		nc++;
+		//Using the unformated output to print the character in the terminal
+		//putchar(c);
+	}	
+	printf("\n");
+	printf("%d", nc);
+}
