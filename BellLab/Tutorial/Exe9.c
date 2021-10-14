31e66c21f2ab17db659d00399d7ba369f70b864f
collins <collinsboniface1000@gmail.com>
2021-10-14 05:09:28 +0300

    The second version of the counting characters program that uses the less variables

---
+++ BellLab/Tutorial/Exe9.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+	//The secound version of the Counting program 
+	double nc; 
+	for (nc = 0; getchar() != EOF; nc++) {
+		;
+	}
+	printf("\n");
+	printf("%.0f\n", nc);
+}
