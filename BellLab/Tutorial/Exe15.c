0317ff68f24b86ab54b7834c17d8b7f8ceb690da
collins <collinsboniface1000@gmail.com>
2021-10-14 07:23:39 +0300

    This program takes the input and prints the output one word per line

---
+++ BellLab/Tutorial/Exe15.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+	//A program that takes it's input and prints it's output one word per line 
+	int c;
+	while((c = getchar()) != EOF) {
+		if(c == '\n' c == ' ' || c == '\t') {
+			putchar('\n');
+		}
+		else 
+			putchar(c);
+	}
+
+}
