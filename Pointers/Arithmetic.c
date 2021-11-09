8834e74432b7ecda5db43b5263b8282b7288bdca
collins <collinsboniface1000@gmail.com>
2021-11-09 12:14:08 +0200

    Updating the Arithmetic.c file by adding the Addtion of the pointer in the arrays

--- Pointers/Arithmetic.c
+++ Pointers/Arithmetic.c
@@ -1,5 +1,25 @@
 #include <stdio.h>
 
 int main() {
+	//Addtion in Pointer
+	//You cann't perform addtion in the two pointer variable invalide (p+q)
+	//p + 1 -> This is valid: Formula: (base address + 1 * sizeof(datatype))
+	//This will be usefull when we a dealing with array and not the variables
+	/*
+	int a = 10;
+	int *p = &a;
+	//int *q = &a; <- Invalid for adding the two pointer
+	p = p + 2;
+	*/
+
 	
+	int a[5] = {0, 1, -1, 10, 1};
+	int *p = &a[0];
+	p = p + 2;
+	printf("%d \n", *p);
+	p = p+1;
+	*p = 2;
+	printf("%d", *p);
+	
+
 }
