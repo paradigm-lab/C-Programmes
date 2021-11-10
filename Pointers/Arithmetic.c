c0be12fbb03ba10e8dda2542e86cc7b34b0d41fd
collins <collinsboniface1000@gmail.com>
2021-11-10 14:56:56 +0200

    Updating the Arithmetic.c file by adding the subtraction of the pointer whereby we so that we can subtract two pointers and get an integer and when we subtracte a pointer and an integer we get a pointer

--- Pointers/Arithmetic.c
+++ Pointers/Arithmetic.c
@@ -10,7 +10,7 @@ int main() {
 	int *p = &a;
 	//int *q = &a; <- Invalid for adding the two pointer
 	p = p + 2;
-	*/
+	
 
 	
 	int a[5] = {0, 1, -1, 10, 1};
@@ -20,6 +20,57 @@ int main() {
 	p = p+1;
 	*p = 2;
 	printf("%d", *p);
-	
+	*/
 
+
+	//Subtraction in Pointer
+	//p - q and p - 2 this is possible for the subtraction of the pointer this is because in subtraction of the pointers,
+	//It returns an interger and not the address like in addition of the pointer. but 
+	//If your subtracte a pointer to an integer the it returns a pointer.
+	
+	int d;
+	//int b[] = {1, 2, 4};
+	int a[5] = {50, 1, -1, 10, 11};
+	int *p = a;
+	//int *q = &b;
+	int *q = &a[4];
+	d = q - p;
+	//This will return the number of the element between the two pointers
+	//p = 1000 and p = 1012 -> 1000 - 1012 = -12/sizeof(data_type)
+	printf("%d \n", d);
+
+	q = q - 2;
+	//printf("%d", *q); -> The answer is -1 That is in index 2
+	
+	//This will return the a pointer	
+	//Formula: Address of q - 2 * sizeof(datatype)
+	//q - n = q - n * sizeof(datatype)
+	
+	//d = p - q;
+	//printf("%d\n", d) -> The value is -2 and that is the range between p and q
+
+	//Assignment:
+	*q = 25;
+	printf("%d \n", *q);
+	
+	d = q - p;
+	printf("%d \n", d);
+
+	*p = 27;
+	printf("%d\n", *p);
+	printf("%d\n", a[0]);
+
+	q = q - 3;
+	//printf("%d\n", *q); -> We will get a gabage value that is out of the array
+	
+	p = p + 3;
+	printf("%d \n", *p);
+	printf("%d \n", a[3]);
+
+	d = p - q;
+	printf("%d \n", p);
+	printf("%d \n", q);
+	printf("%d", d);
+
+	
 }
