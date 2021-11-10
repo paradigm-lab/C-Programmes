d3f956032bc6ae1c0a10ee721bf17b01a61c02b5
collins <collinsboniface1000@gmail.com>
2021-11-10 16:48:55 +0200

    Updating the Assigment.c file by adding the comment to make more clarification

--- Pointers/Assigment.c
+++ Pointers/Assigment.c
@@ -5,12 +5,13 @@ int main() {
 	int a = 10, b = 11;
 	int *p, *q;
 
-	//This is intialization of the pointers here both p and q they are pointing to one address and printing the values by using the astric or the indirectional operator to derefference the value
+	//This is intialization of the pointers here both p and q they are pointing to one address and printing the values by using the astric or the indirectional operator to derefference the valu
 	p = &a;
 	q = p;
 	printf("a = %d %d %d \n", a, *p, *q);
 	
-	//In this case we are accessing the values
+	//In this case we are accessing the values using the pointer, 
+	//We use the indirectional operator or the astric operator
 	q = &b;
 	*q = *p;
 	printf("b = %d %d %d", a, *p, *q);
