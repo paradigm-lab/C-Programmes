34656a7035622248e1457ab60a1215058bf78ee0
collins <collinsboniface1000@gmail.com>
2021-11-25 17:50:16 +0200

    Creating a simple function definition and function calling and how the function is been executed or displayed in the main memory

---
+++ functions/function1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+// Function Definition.
+// This are user defined functions.
+void sum() {
+	int a, b, sum = 0;
+	print("Enter two numbers: ");
+	scanf("%d %d", &a, &b);
+	sum = a + b;
+	printf("The sum is: %d \n", sum);
+}
+
+int main() {
+	
+	// Function calling
+	// When the function is done for the execution then, 
+	// The function is been demolished from the memory.
+	sum();
+	printf("Hello More calculation.");
+	
+	
+	// After the control of the execuation of the program is compeleted 
+	// Then the memory is been freeed or released from the main memory.
+	sum();
+	printf("Hello");
+
+	return 0;
+}
