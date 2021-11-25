6cbf59ea6262e8d3ece722d171ee938c42dd5279
collins <collinsboniface1000@gmail.com>
2021-11-25 18:00:39 +0200

    Running the function1.c file and it works as expected in calling the function and defining the function

--- functions/function1.c
+++ functions/function1.c
@@ -4,7 +4,7 @@
 // This are user defined functions.
 void sum() {
 	int a, b, sum = 0;
-	print("Enter two numbers: ");
+	printf("Enter two numbers: ");
 	scanf("%d %d", &a, &b);
 	sum = a + b;
 	printf("The sum is: %d \n", sum);
@@ -16,13 +16,15 @@ int main() {
 	// When the function is done for the execution then, 
 	// The function is been demolished from the memory.
 	sum();
-	printf("Hello More calculation.");
+	printf("Hello More calculation. \n");
 	
 	
 	// After the control of the execuation of the program is compeleted 
 	// Then the memory is been freeed or released from the main memory.
 	sum();
-	printf("Hello");
+	printf("Hello \n");
+
+	sum();
 
 	return 0;
 }
