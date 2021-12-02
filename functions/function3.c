40c8dc39c6609e288033e27dfe3afa585656a1ce
collins <collinsboniface1000@gmail.com>
2021-12-02 18:42:44 +0200

    Adding the explanation about the function definition, initialization and calling

--- functions/function3.c
+++ functions/function3.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
-//Function Declaration
+/* 			Function Declaration
+ * We have got the return type, the function name and the function argument
+ * In function argument we have an optional if the function takes the arguments you can 
+ * only write the data types.
+ * Note: It's not necessary to write the name of the variable in the function arguments
+ */
 void sum(void);
 
 int main() {
@@ -8,11 +13,25 @@ int main() {
 	/*Function calling 
 	 * When function is called then the control goes to the function definition and it 
 	 * compaies the function definition and the funtion calling
+	 *
 	 */
 
 	sum();
 
 }
+
+/* The function has got two main parts Which are:
+ * 	Function header and Function body
+ *
+ * In Function header we have got the return type, function name and the function arguments 
+ * with there data types.
+ *		AND
+ * In Function body we have got the function logic whereby we can have the local variables,
+ * function calling and the return statement
+ *
+ * When we don't have the return statement the closing brace will act as the void return type.
+ */
+
 
 //Function Definition
 void sum() {
@@ -21,6 +40,6 @@ void sum() {
 	scanf("%d %d", &a, &b);
 
 	sum = a + b;
-	printf("Sum:%d", sum);
+	printf("Sum:%d \n", sum);
 
 }
